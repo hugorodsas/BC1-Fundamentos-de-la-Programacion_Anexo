0e7c4fa3663bc1a5d203e599de6b0fd18105d505
hugorodsas <hugo.rodsas@educa.jcyl.es>
2025-10-15 12:20:13 +0200

    Ejercicio Matriz

---
+++ Matriz3x3.java
@@ -0,0 +1,11 @@
+public class Matriz3x3 {
+    public static void main(String[] args) {
+    int[][] numeros={{2,4,6},{3,6,9},{5,10,15}};
+     for(int i=0;i<numeros.length;i++){
+        for(int j=0;j<numeros[i].length;j++){
+            System.out.print(numeros[i][j] + " ");
+        }
+    System.out.println("");
+     }
+    }
+}
